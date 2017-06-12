62842456b70f72540a886ce5bceddb89108f28c8
Dan <Dan@Yufi>
2017-06-12 12:59:27 -0400

    Jsoup

--- src/Class.java
+++ src/Class.java
@@ -6,6 +6,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Vector;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class Class {
 	private String classID;
 	private String className;
@@ -22,13 +26,44 @@ public class Class {
 
 		searchURL = "http://www.freepatentsonline.com/result.html?p=1&srch=xprtsrch&query_txt=" + classID.replaceAll("/", "%2F") + "&uspat=on&usapp=on&eupat=on&pct=on&date_range=all&stemming=on&sort=relevance&search=Search";	
 		
+		System.out.println(searchURL);
+		
 		URL url;
 		InputStream is = null;
 		BufferedReader br = null;
 	    String line;
 	    
+	    //try {
+			//Document doc = Jsoup.connect(searchURL).get();
+			
+			//System.out.println(doc.toString());
+			
+			//Elements elements = doc.select("div[class=well well-small]");
+			//System.out.println(elements.size());
+			//System.out.println(elements.first().toString());
+			
+			
+			
+			//Elements elements = doc.select("div[class=legacy-container]");
+			//System.out.println("# of elements: " + elements.size());
+			//System.out.println(elements.get(0).select("a[href]").attr("abs:href").toString());
+			
+			
+			
+			
+			
+		//} catch (IOException e) {
+			// TODO Auto-generated catch block
+		//	e.printStackTrace();
+		//}
+	    
+	    
+
 		try {
 			url = new URL(searchURL);
+			
+			//System.out.println(url.toString());
+			
 			is = url.openStream();  // throws an IOException
 			br = new BufferedReader(new InputStreamReader(is));
 			
@@ -36,20 +71,26 @@ public class Class {
 			boolean done = false;
 	        while (!done) {
 				line = br.readLine();
-				if (line == null)
+				//System.out.println(line);
+				if (line == null) {
 					done = true;
+					//System.out.println("#####");
+				}
 				else if (line.contains("Matches 1 - 50 out of")) {
 					String count = line.substring(line.indexOf('f') + 1, line.indexOf('<') - 1);
 	        		count = count.replaceAll(" ","");
 	        		
 	        		//System.out.println(count);
 	        		resultCount = Integer.parseInt(count);
-	        		System.out.println(resultCount);
+	        		//System.out.println(resultCount);
 	        		
 	        		Vector<String> resultPageURLs = new Vector<String>();
 	        		
 	        		// Generate resultPageURLs
-	        		for (int i = 0; i < resultCount / 50 + resultCount % 50 /*1*/; i++) {
+	        		int resultPageCount = resultCount / 50;
+	        		if (resultCount % 50 != 0)
+	        			resultPageCount++;
+	        		for (int i = 0; i < resultPageCount; i++) {
 	        			if (i == 0)
 	        	    		resultPageURLs.add(searchURL);
 	        	    		else {
@@ -58,6 +99,8 @@ public class Class {
 	        	    			//add urlString
 	        	    		}
 	        		}
+	        		
+	        		System.out.println(resultPageURLs.size() + " pages of results");
 		        		
 		        		
 	        		// Fetch URLs from all results pages
@@ -71,17 +114,20 @@ public class Class {
 	        	        while (!done2) {
 	        	        	line = br.readLine();
 	        	        	if (line == null) {
+	        	        		//System.out.println("@@@@@");
 	        	        		done2 = true;
 	        	        	}
 	        	        	else if (line.contains("legacy-container")) {
-	        	        		//System.out.println(num + " !!!!!!!!!!!!!!!");
-	        	        		while (!(line = br.readLine()).contains("</table>")/* && num < 11*/) {
+	        	        		//System.out.println("!!!!!!!!!!!!!!!");
+	        	        		while (!(line = br.readLine()).contains("</table>")) {
 	        	        			if (line.contains("href")) {
 	        	        				String suburl = line.substring(line.indexOf('=') + 3, line.indexOf('>') - 1);
 	        	        				
+	        	        				//System.out.println(suburl);
+	        	        				
 	        	        				patentURLs.add("http://www.freepatentsonline.com/" + suburl);
 	        	        				done2 = true;
-	        	        				System.out.println(patentURLs.size() + ": " + patentURLs.get(patentURLs.size() - 1));
+	        	        				//System.out.println(patentURLs.size() + ": " + patentURLs.get(patentURLs.size() - 1));
 	        	        				num++;
 	        	        			}
 	        	        		}
@@ -91,6 +137,22 @@ public class Class {
 		        	
 		        	done = true;
 	        	}
+				else if (line.contains("legacy-container")) {
+	        		//System.out.println("!!!!!!!!!!!!!!!");
+	        		boolean done2 = false;
+	        		while (!(line = br.readLine()).contains("</table>")) {
+	        			if (line.contains("href")) {
+	        				String suburl = line.substring(line.indexOf('=') + 3, line.indexOf('>') - 1);
+	        				
+	        				//System.out.println(suburl);
+	        				
+	        				patentURLs.add("http://www.freepatentsonline.com/" + suburl);
+	        				//done2 = true;
+	        				//System.out.println(patentURLs.size() + ": " + patentURLs.get(patentURLs.size() - 1));
+	        			}
+	        		}
+	        	}
+				
 	        }
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
--- src/Patent.java
+++ src/Patent.java
@@ -5,6 +5,12 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Vector;
 
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class Patent {
 	URL url;
 	String numberStr;
@@ -24,58 +30,201 @@ public class Patent {
 	    String line;
 	    boolean done = false;
 	    
+	    
+	    
+	    
+	    
 		try {
-			url = new URL(urlString);
-			is = url.openStream();  // throws an IOException
-			br = new BufferedReader(new InputStreamReader(is));
+			//url = new URL(urlString);
+			//is = url.openStream();  // throws an IOException
+			//br = new BufferedReader(new InputStreamReader(is));
+			Elements elements;
 			
 			/// Main
 			// ID number
-			done = false;
-			while (!(line = br.readLine()).contains("<input type=\"hidden\" name=\"patent\"")) {
+			
+			Document doc = Jsoup.connect(urlString).get();
+			
+			elements = doc.select("form[name=biblio]");
+			Elements elements3 = elements.select("input[name=patent]");
+			numberStr = elements3.first().attr("value");
+			
+			
+			elements = doc.select("div[class=disp_doc2]");
+			
+			for (int i = 0; i < elements.size(); i++) {
+				String eTitle = "no title";
+				String eText = "no text";
+				
+				Elements elements2 = elements.get(i).select("div[class=disp_elm_title]");
+				if (elements2.size() != 0) {
+					eTitle = elements2.select("div[class=disp_elm_title]").first().text();
+				}
+				
+				elements2 = elements.get(i).select("div[class=disp_elm_text]");
+				if (elements2.size() != 0) {
+					eText = elements2.select("div[class=disp_elm_text]").first().text();
+				}
+				
+				if (eTitle.equals("")) {
+					
+				}
+				else if (eTitle.equals("Title:")) {
+					titleStr = eText;
+				}
+				else if (eTitle.equals("Abstract:")) {
+					abstractStr = eText;
+				}
+				else if (eTitle.equals("Inventors:")) {
+					 = eText;
+				}
+				else if (eTitle.equals("Application Number:")) {
+					 = eText;
+				}
+				else if (eTitle.equals("Publication Date:")) {
+					 = eText;
+				}
+				else if (eTitle.equals("Filing Date:")) {
+					 = eText;
+				}
+				else if (eTitle.equals("Asignee:")) {
+					 = eText;
+				}
+				else if (eTitle.equals("Primary Class:")) {
+					 = eText;
+				}
+				else if (eTitle.equals("")) { // Other classes
+					 = eText;
+				}
+				else if (eTitle.equals("International Classes:")) {
+					 = eText;
+				}
+				else if (eTitle.equals("")) {
+					 = eText;
+				}
+				
+				
+				
+				
+				
+				
+				// Images
+				
+				// Assignee
+				
+				
+				
+				// Attorneys/Agents
+				
+				// App Num
+				
+				// Pub Date
+				
+				// File Date
+				
+				// Referenced by
+				
+				// Primary Class
+				
+				// International Classes
+				
+				// Field of Search
+				
+				// Primary Examiner
 
+				
+				/// Claims
+				
+				/// Description
+				
+				/// Citations
+				
+				/// PDF
+				
+				
+				
+				//System.out.println(eTitle);
+				//System.out.println(eText);
 			}
 			
-			numberStr = line.substring(line.indexOf("ue=\"") + 4, line.indexOf("\"/>"));
+			System.out.println(numberStr + ": " + titleStr);
+			//System.out.println(doc.toString());
+			
+			//Elements elements = doc.select("div[class=well well-small]");
+			//System.out.println(elements.size());
+			//System.out.println(elements.first().toString());
+			
+			
+			
+			//Elements elements = doc.select("div[class=legacy-container]");
+			//System.out.println("# of elements: " + elements.size());
+			//System.out.println(elements.get(0).select("a[href]").attr("abs:href").toString());
+			
+		
+
+			//done = false;
+			//while (!(line = br.readLine()).contains("<input type=\"hidden\" name=\"patent\"")) {
+
+			//}
+			
+			//numberStr = line.substring(line.indexOf("ue=\"") + 4, line.indexOf("\"/>"));
 			//if (titleStr == "")
 				//titleStr = "no title";
 			//System.out.println(numberStr);
 			
 			// Title - 207
-			done = false;
-			while (!(line = br.readLine()).contains("<input type=\"hidden\" name=\"title\"")) {
+			//done = false;
+			//while (!(line = br.readLine()).contains("<input type=\"hidden\" name=\"title\"")) {
 
-			}
+			//}
 			
-			titleStr = line.substring(line.indexOf("ue=\"") + 4, line.indexOf("\"/>"));
+			//titleStr = line.substring(line.indexOf("ue=\"") + 4, line.indexOf("\"/>"));
 			//if (titleStr == "")
 				//titleStr = "no title";
-			System.out.println(titleStr);
+			//System.out.println(titleStr);
 			
 			/// Biblio
 			// Abstract - <!--	Abstract  -->
-			done = false;
-			while (!(line = br.readLine()).contains("<!--\tAbstract  -->")) {
+			//done = false;
+			//while (!(line = br.readLine()).contains("<!--\tAbstract  -->")) {
 				
-			}
+			//}
 			//System.out.println(line);
-			done = false;
+			//done = false;
 			
-			int num = 0;
-			while (!done) {
-				line = br.readLine();
-				if (num == 3) {
-					System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-					done = true;
-				}
-				else if (line.contains("<div class=\"span"))
-					abstractStr = line.substring(line.indexOf('>') + 1, line.indexOf("</"));
-				System.out.println(line);
-				num++;
-			}
+			//int num = 0;
+			//while (!done) {
+			//	line = br.readLine();
+			//	if (line == null) {
+					//System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			//		done = true;
+			//	}
+			//	else if (line.contains("<div class=\"disp_elm_text\">")) {
+			//		line = br.readLine();
+			//		//System.out.println(line);
+			//		
+			//		// Delete </div> at end
+			//		line = line.replaceAll("</div>", "");
+			//		
+			//		// Delete leading and trailing whitespaces
+			//		line = line.trim();
+					
+					
+					
+					
+					
+					// Delete text augmentations (highlight, bold, etc...)
+			//		abstractStr = line;
+					//.substring(1, 3 /*line.indexOf("</")*/);
+			//		done = true;
+			//	}
+					
+				//System.out.println(line);
+			//	num++;
+			//}
 			
-			if (abstractStr == null)
-				abstractStr = "ABSTRACT";
+			//if (abstractStr == null)
+			//	abstractStr = "ABSTRACT";
 			
 			
 			//System.out.println(abstractStr);
